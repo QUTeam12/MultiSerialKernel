7aaa7bed0e446f570b508c64fc5d9bd306438c6a
Kaito Shigyo / 執行凱斗 <132188853+furedea@users.noreply.github.com>
2024-12-19 22:28:32 +0900

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-	while(1) {
-		char c[256];
-		scanf("%s", c);
-		printf("%s", c);
-	}
-	return 0;
+    while (1) {
+        char c[256];
+        scanf("%s", c);
+        printf("%s", c);
+    }
+    return 0;
 }
