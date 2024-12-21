860079ae780268b5c2120c184447372458aea88f
Kaito Shigyo / 執行凱斗 <132188853+furedea@users.noreply.github.com>
2024-12-21 15:03:29 +0900

    Update csys68k.c

--- csys68k.c
+++ csys68k.c
@@ -1,21 +1,10 @@
-#include <errno.h>
 #include <fcntl.h>
 #include <stdarg.h>
 
 extern void outbyte(int port, unsigned char c);
 extern char inbyte(int port);
 
-void out_msg(int port, const char* msg) { // msgのNULL判定はfprintfインクルード不可なので面倒 
-	if (msg == NULL) {
-		return;
-	}
-	while (*msg != '\0') {
-		outbyte(port, *msg);
-		msg++;
-	}
-}
-
-int readTask(int port, char* buf, int nbytes) {
+int read_line(int port, char* buf, int nbytes) {
     char c;
     int i;
     for (i = 0; i < nbytes; i++) {
@@ -24,16 +13,8 @@ int readTask(int port, char* buf, int nbytes) {
             case '\r':  // CRの場合
             case '\n':  // LFの場合
                 outbyte(port, '\r');
-                outbyte(port, '\n'); 
+                outbyte(port, '\n');
                 *(buf + i) = '\n';
-                return i + 1;
-            case '\x1b':  // Escの場合
-                out_msg(port, "\r\n\r\n:Command Mode: You have some options.\r\n");
-                out_msg(port, "[Enter] Finish editing\r\n");
-                out_msg(port, "[w] Restart Write Mode\r\n");
-                out_msg(port, "[r] Read Mode\r\n");
-                out_msg(port, "#Attention# The user in Write mode is only one.\r\n\r\n");
-                *(buf + i) = '\v';
                 return i + 1;
             case '\x7f':  // バックスペースの場合
                 if (i > 0) {
@@ -43,7 +24,7 @@ int readTask(int port, char* buf, int nbytes) {
                     i--;
                 }
                 i--;
-                continue;
+                break;
             default:  // 通常文字の場合
                 outbyte(port, c);
                 *(buf + i) = c;
@@ -58,19 +39,18 @@ int read(int fd, char* buf, int nbytes) {
     switch (fd) {
         case 0:
         case 3:
-            i = readTask(0, buf, nbytes);
+            i = read_line(0, buf, nbytes);
             break;
         case 4:
-            i = readTask(1, buf, nbytes);
+            i = read_line(1, buf, nbytes);
             break;
         default:
             return EBADF;
     }
-	return i;
+    return i;
 }
 
-
-void writeTask(int port, char* buf, int nbytes) {
+void write_task(int port, char* buf, int nbytes) {
     int i, j;
     for (i = 0; i < nbytes; i++) {
         if (*(buf + i) == '\n') {
@@ -86,17 +66,16 @@ int write(int fd, char* buf, int nbytes) {
         case 1:
         case 2:
         case 3:
-            writeTask(0, buf, nbytes);
+            write_task(0, buf, nbytes);
             break;
         case 4:
-            writeTask(1, buf, nbytes);
+            write_task(1, buf, nbytes);
             break;
         default:
             return EBADF;
     }
     return nbytes;
 }
-
 
 int fcntl(int fd, int cmd, ...) {
     if (cmd == F_GETFL) {
