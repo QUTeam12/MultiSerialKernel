b1bf6bb2857fe4c4506220e26b31c6ef5bfeb3f0
Kaito Shigyo / 執行凱斗 <132188853+furedea@users.noreply.github.com>
2024-12-21 15:04:01 +0900

    Update file.h

--- file.h
+++ file.h
@@ -1,3 +1,15 @@
+#pragma once
+/*
+だめな場合
+#ifndef IN_H
+#define IN_H
+...
+#endif
+*/
+
+#include <stddef.h>  // size_t
+#include <stdio.h>   // FILE
+
 #define NULL_SEMAPHORE_ID -1
 #define NUM_FILE 8
 #define UNDEFINED_SIZE -1
@@ -12,13 +24,19 @@ typedef struct {
         int semaphore_id;  // 排他制御用セマフォID
 } FILE_ENTRY;
 
-void print_file_table();
+extern void outbyte(int port, unsigned char c);
+extern char inbyte(int port);
+
+void print_file_table(FILE* w_stream);
 void init_file_table();
-void touch(const char* filename);
-void check_null(const char* ptr);
-void copy_string(const char* from, char* to, int to_size);
-void edit(const char* filename);
+void touch(const char* filename, FILE* w_stream);
+void copy_string(const char* from, char* to, size_t to_size);
+void rm(const char* filename, FILE* w_stream);
 FILE_ID_TYPE search_file_id(const char* filename);
-void write_lines(char* buffer);
-void write_to_file(const char* buffer, FILE_ID_TYPE id);
-void rm(const char* filename);
+void edit(const char* filename, FILE* r_w_stream);
+void write_to_file(FILE* r_w_stream, FILE_ID_TYPE id);
+int get_port(FILE* stream);
+void out_msg(int port, const char* msg);
+void check_null(const char* ptr);
+void check_size(size_t to_size);
+void check_fd(int fd);
