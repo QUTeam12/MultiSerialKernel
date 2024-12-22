320cca5e0f1c1377e40adae1e895062547169377
Kaito Shigyo / 執行凱斗 <132188853+furedea@users.noreply.github.com>
2024-12-22 14:52:18 +0900

    [add]inbyte_or

--- inchrw.s
+++ inchrw.s
@@ -1,5 +1,6 @@
 .include	"equdefs.inc"
-.global inbyte
+.global	inbyte
+.global	inbyte_or
 
 .section .bss
 .even
@@ -8,7 +9,7 @@
 .even
 
 ********************
-** inbyte: 
+** inbyte:
 ** 入力: fd(long, 4bytes): チャンネルの指定
 ** 出力: char型1文字データ(%d0)
 *********************
@@ -17,20 +18,57 @@ inbyte:
 	move.l	%sp, %a0
 	add.l	#20, %a0
 	sub.l	#4, %sp
-	bra	loop
-loop:
-	move.l	#SYSCALL_NUM_GETSTRING, %d0	
+	bra	inbyte_loop
+inbyte_loop:
+	move.l	#SYSCALL_NUM_GETSTRING, %d0
 	move.l	(%a0), %d1
 	move.l	%sp, %d2
-	move.l	#1, %d3	
+	move.l	#1, %d3
 	trap	#0
 
 	cmpi.l	#1, %d0
-	bne	loop
-	
+	bne	inbyte_loop
+
 	move.b	(%sp), %d0
 	add.l	#4, %sp
-	
+
 	movem.l	(%sp)+, %d1-%d3/%a0
 	rts
-	
+
+********************
+** inbyte_or: inbyteしているが文字がない場合は0を返す
+** 入力: fd(long, 4bytes): チャンネルの指定
+** 出力: char型1文字データ(%d0)
+********************
+inbyte_or:
+	movem.l	%d1-%d4/%a0, -(%sp)
+	move.l	%sp, %a0
+	add.l	#24, %a0
+	sub.l	#4, %sp
+	bra	inbyte_loop
+inbyte_or_loop:
+	add.l	#1, %d4
+	move.l	#SYSCALL_NUM_GETSTRING, %d0
+	move.l	(%a0), %d1
+	move.l	%sp, %d2
+	move.l	#1, %d3
+	trap	#0
+
+	cmpi.l	#1, %d0
+	bne	inbyte_or_check
+
+	move.b	(%sp), %d0
+	add.l	#4, %sp
+
+	movem.l	(%sp)+, %d1-%d4/%a0
+	rts
+
+inbyte_or_check:
+	cmpi.l	#100, %d4
+	bne	inbyte_or_loop
+
+	move.l	#0, %d0
+	add.l	#4, %sp
+
+	movem.l	(%sp)+, %d1-%d4/%a0
+	rts
