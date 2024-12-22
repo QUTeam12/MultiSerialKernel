34ab53b7365fc3c6597e847c2c78e20e58c2c12a
Kaito Shigyo / 執行凱斗 <132188853+furedea@users.noreply.github.com>
2024-12-22 14:51:44 +0900

    Update file.h

--- file.h
+++ file.h
@@ -14,7 +14,7 @@
 #define NUM_FILE 8
 #define UNDEFINED_SIZE -1
 
-typedef int FILE_ID_TYPE;
+typedef unsigned int FILE_ID_TYPE;
 
 typedef struct {
         char name[16 + 1];  // ファイル名(確保するバイトに終端文字\0を含むため17バイトで16文字まで)
@@ -26,17 +26,19 @@ typedef struct {
 
 extern void outbyte(int port, unsigned char c);
 extern char inbyte(int port);
+extern char intbyte_or(int port);
 
 void print_file_table(FILE* w_stream);
 void init_file_table();
+void select_command(FILE* r_w_stream);
 void touch(const char* filename, FILE* w_stream);
 void copy_string(const char* from, char* to, size_t to_size);
 void rm(const char* filename, FILE* w_stream);
 FILE_ID_TYPE search_file_id(const char* filename);
 void edit(const char* filename, FILE* r_w_stream);
-void write_to_file(FILE* r_w_stream, FILE_ID_TYPE id);
+void write_mode(FILE* r_w_stream, FILE_ID_TYPE id);
+void read_mode(FILE* r_w_stream, FILE_ID_TYPE id);
 int get_port(FILE* stream);
-void out_msg(int port, const char* msg);
+void out_no_newline_end(int port, const char* buf, const unsigned int buf_size);
+void input(char* buf, size_t buf_size);
 void check_null(const char* ptr);
-void check_size(size_t to_size);
-void check_fd(int fd);
